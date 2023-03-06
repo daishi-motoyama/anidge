5a7b6d79da465433ef5b9a1be5cfc48024ab1ad7
motoyama-daishi <48724872dad@gmail.com>
2023-03-06 22:07:22 +0900

    add: backend basez

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,19 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    // GraphQLModule.forRoot<ApolloDriverConfig>({
+    //   driver: ApolloDriver,
+    //   autoSchemaFile: join(process.cwd(), 'src/graphql/schema/guest.schema.gql'),
+    //   cors: {
+    //     origin: '*',
+    //   },
+    //   path: '/graphql/guest',
+    //   sortSchema: true,
+    // }),
+  ],
 })
 export class AppModule {}
